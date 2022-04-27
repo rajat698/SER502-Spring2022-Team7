c7a8c53e292f6bb02ac7e2749530b7041d4503a4
vsingh57 <vsingh57@asu.edu>
2022-04-27 16:53:18 -0700

    Added all parser rules

--- src/compiler/parser.pl
+++ src/compiler/parser.pl
@@ -24,6 +24,7 @@ datatype(int).
 datatype(str).
 datatype(bool).
 
+
 boolean('True').
 boolean('False').
 
@@ -86,28 +87,48 @@ bool_expr3(T) --> bool(T).
 bool(T) --> [T], {boolean(T)}.
 
 % statements
-stmt_list(none) --> [].
-stmt_list(stmt_list(T1,T2)) --> stmt(T1), stmt_list(T2).
+stmt_list(T) --> stmt(T), [';'].
+stmt_list(T) --> stmt_block(T).
+stmt_list(stmt_list(T1,T2)) --> stmt(T1), [';'], stmt_list(T2).
+stmt_list(stmt_list(T1,T2)) --> stmt_block(T1), stmt_list(T2).
 
+
+
+% declarations
 value(T) --> bool_expr(T).
 value(T) --> expr(T).
 value(T) --> string(T).
+stmt(dec(T1,T2)) --> [T1], id(T2), {datatype(T1)}.
+stmt(decAssign(T1,T2,T3)) --> [T1], id(T2), [=], value(T3), {datatype(T1)}.
 
-% declarations
-stmt(dec(T1,T2)) --> [T1], id(T2), [;], {datatype(T1)}.
-stmt(decAssign(T1,T2,T3)) --> [T1], id(T2), [=], value(T3), [;], {datatype(T1)}.     
+% display
+stmt(display(T)) --> [display], value(T).
 
 % assignment
-stmt(assign(T1,T2)) --> id(T1), [=], value(T2), [;].
+stmt(assign(T1,T2)) --> id(T1), [=], value(T2).
+
+% ternary if-else
+stmt(ifelse(T1,T2,T3)) --> bool_expr(T1), ['?'], stmt(T2), [':'], stmt(T3), [';'].
 
 % if-else block
-stmt(ifelse(T1,T2,T3)) --> bool_expr(T1), ['?'], stmt_list(T2), [':'], stmt_list(T3).
-stmt(ifelse(T1,T2,T3)) --> [if], 
+stmt_block(ifelse(T1,T2,T3)) --> [if], 
     ['('], bool_expr(T1), [')'], 
     ['{'], stmt_list(T2), ['}'],
     [else],
     ['{'], stmt_list(T3), ['}'].
 
+% for-loop traditional
+stmt_block(forT(T1,T2,T3,T4)) --> [for-loop, '('], 
+    stmt(T1), [';'], bool_expr(T2), [';'], stmt(T3), [')', '{'],
+    stmt_list(T4), [')'].
+
+% for-loop range
+stmt_block(forR(T1,T2,T3,T4)) --> [for-loop, '('],
+    id(T1), [in, range, '('], expr(T2), [','] expr(T3), [')', '{'],
+    stmt_list(T4), [')'].
+
+% program
+program(T) --> [shuru], stmt_list(T), [khatam].
 
     
 
