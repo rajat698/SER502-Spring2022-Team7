8963341a71d25e9b055d4092af8434037a727bce
vsingh57 <vsingh57@asu.edu>
2022-04-27 09:02:51 -0700

    Added parser rules for id, added id to expressions

--- src/compiler/parser.pl
+++ src/compiler/parser.pl
@@ -1,19 +1,15 @@
-digit(0).
-digit(1).
-digit(2).
-digit(3).
-digit(4).
-digit(5).
-digit(6).
-digit(7).
-digit(8).
-digit(9).
-
 :-use_rendering(svgtree).
-:-table expr/3, expr2/3.
+:-table expr/3, expr2/3, expr3/3.
 
-num(N) --> [N], {number(N)}.
+num(N1) --> [N], {atom_number(N, N1)}.
 %num(num(T, N)) --> num(T), [N], {digit(N)}.
+
+id(id(T)) --> [T], {atom_chars(T, TList), isIdentifier(TList, [])}.
+isIdentifier --> [A], id_suffix, {is_alpha(A)}.
+id_suffix --> [].
+id_suffix --> [A], id_suffix, {is_alpha(A)}.
+id_suffix --> [N], id_suffix, {atom_number(N, _)}.
+
 
 expr(+(T1,T2)) --> expr(T1), ['%'], expr2(T2).
 expr(T) --> expr2(T).
@@ -22,10 +18,12 @@ expr2(+(T1,T2)) --> expr2(T1), [+], expr3(T2).
 expr2(-(T1,T2)) --> expr2(T1), [-], expr3(T2).
 expr2(T) --> expr3(T).
 
-expr3(*(T1,T2)) --> expr2(T1), [*], expr4(T2).
-expr3(/(T1,T2)) --> expr2(T1), [/], expr4(T2).
+expr3(*(T1,T2)) --> expr3(T1), [*], expr4(T2).
+expr3(/(T1,T2)) --> expr3(T1), [/], expr4(T2).
 expr3(T) --> expr4(T).
 
 expr4(T) --> num(T).
-%expr3(T) --> id(T).
+expr4(T) --> id(T).
 expr4(T) --> ['('], expr(T) , [')'].
+
+
