3bfcf9a98710b5038d869505f30e344621f18e38
vsingh57 <vsingh57@asu.edu>
2022-04-27 11:44:02 -0700

    Added parser rules for boolean expressions

--- src/compiler/parser.pl
+++ src/compiler/parser.pl
@@ -1,3 +1,26 @@
+spec_char('!').
+spec_char('\\').
+spec_char('#').
+spec_char('$').
+spec_char('&').
+spec_char('(').
+spec_char(')').
+spec_char('*').
+spec_char('+').
+spec_char(',').
+spec_char('-').
+spec_char('.').
+spec_char('/').
+spec_char(':').
+spec_char(';').
+spec_char('<').
+spec_char('=').
+spec_char('>').
+spec_char('?').
+spec_char('@').
+spec_char('~').
+
+
 boolean('True').
 boolean('False').
 
@@ -6,6 +29,13 @@ boolean('False').
 
 % numbers
 num(N1) --> [N], {atom_number(N, N1)}.
+
+% strings
+string(str(T)) --> ['\"'], [T], ['\"'], {atom_chars(T, TList), char(TList, [])}.
+char --> [].
+char --> [A], char, {is_alpha(A)}.
+char --> [A], char, {atom_number(A, _)}.
+char --> [A], char, {spec_char(A)}.
 
 % identifiers
 id(id(T)) --> [T], {atom_chars(T, TList), isIdentifier(TList, [])}.
@@ -36,15 +66,23 @@ bool_expr(T) --> bool_expr2(T).
 bool_expr2(and(T1,T2)) --> bool_expr2(T1), [and], bool_expr3(T2).
 bool_expr2(T) --> bool_expr3(T).
 
-% ==
+% equality (boolean)
+value(T) --> bool_expr(T).
+value(T) --> expr(T).
+value(T) --> string(T).
 bool_expr3(equals(T1,T2)) -->  bool_expr(T1), [==], bool_expr(T2).
+bool_expr3(equals(T1,T2)) --> expr(T1), [==], expr(T2).
+bool_expr3(equals(T1,T2)) --> id(T1), [==], value(T2).
 
+% arithmetic comparison (boolean)
+bool_expr3(lt(T1,T2)) --> expr(T1), [<], expr(T2).
+bool_expr3(gt(T1,T2)) --> expr(T1), [>], expr(T2). 
+bool_expr3(lteq(T1,T2)) --> expr(T1), [<=], expr(T2). 
+bool_expr3(gteq(T1,T2)) --> expr(T1), [>=], expr(T2).
 
 bool_expr3(T) -->['('], bool_expr(T), [')'].
 bool_expr3(not(T)) --> [not], bool_expr(T).
-
-
-% < > 
+bool_expr3(T) --> bool(T).
 bool(T) --> [T], {boolean(T)}.
 
 
